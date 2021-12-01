67fc463128e9e6f951a9b727eea3a9e1b300344f
Rob Johnson <rjohnson@dcs_inc.net>
2021-12-01 11:39:06 -0800

    p1: second solution

--- day1/count_increases.py
+++ day1/count_increases.py
@@ -1,16 +1,21 @@
-import math
+import collections
 
 
-def count_increases(filename):
+def count_increases(filename, window_len=1):
     count = 0
+    last_window = collections.deque(maxlen=window_len)
+    current_window = collections.deque(maxlen=window_len)
     with open(filename, mode="r") as file:
-        last = math.inf  # prevents the first reading from increasing.
         for line in file:
-            if int(line) > last:
+            current_window.append(int(line))
+            if (len(last_window) == window_len) and (
+                sum(current_window) > sum(last_window)
+            ):
                 count += 1
-            last = int(line)
+            last_window.append(int(line))
     return count
 
 
 if __name__ == "__main__":
-    print(count_increases("./input.txt"))
+    print(count_increases("./input.txt"))  # expect 1167
+    print(count_increases("./input.txt", 3))  # expect 1130
