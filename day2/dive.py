e35f34695818d79a8741aa41db0b18e297881ab0
Rob Johnson <rjohnson@dcs_inc.net>
2021-12-02 11:26:15 -0800

    p2 part 2 + example inputs to test.

--- day2/dive.py
+++ day2/dive.py
@@ -1,19 +1,20 @@
 def location(filename):
-    x, y = 0, 0
+    x, y, aim = 0, 0, 0
     with open(filename, mode="r") as file:
         for line in file:
             command, value = line.split()
             value = int(value)
             if command == "forward":
                 x += value
+                y += aim * value
             elif command == "down":
-                y += value
+                aim += value
             elif command == "up":
-                y -= value
+                aim -= value
     return x, y
 
 
 if __name__ == "__main__":
-    result1 = location("input.txt")
-    print(result1)
-    print(f"product = {result1[0] * result1[1]}")
+    result = location("input.txt")
+    print(result)
+    print(f"product = {result[0] * result[1]}")
