519457a50717e1b50b5c97772becfb060504f42d
Rob Johnson <Johnson.rob.alan@gmail.com>
2021-12-04 22:24:49 -0800

    day 5 part 1 solution

--- day5/vents.py
+++ day5/vents.py
@@ -1,2 +1,67 @@
+import dataclasses
+import re
+import collections
+import itertools
+
+
+@dataclasses.dataclass
+class Point:
+    x: int
+    y: int
+
+
+@dataclasses.dataclass
+class Line:
+    start: Point
+    end: Point
+
+    @property
+    def isHorizontal(self):
+        return self.start.y == self.end.y
+
+    @property
+    def isVertical(self):
+        return self.start.x == self.end.x
+
+    @property
+    def isDiagonal(self):
+        return not (self.isVertical or self.isHorizontal)
+
+    """ Returns a list of points that the line will pass through.
+        Not yet adjusted for diagonals """
+
+    def points(self):
+        points = []
+        if self.isHorizontal:
+            increment = 1 if self.start.x < self.end.x else -1
+            for x in range(self.start.x, self.end.x + increment, increment):
+                points.append(f"x{x}y{self.start.y}")
+        if self.isVertical:
+            increment = 1 if self.start.y < self.end.y else -1
+            for y in range(self.start.y, self.end.y + increment, increment):
+                points.append(f"x{self.start.x}y{y}")
+        return points
+
+
+def process(filename):
+    with open(filename) as file:
+        lines = []
+        for line in file:
+            match = re.match(
+                r"(?P<x1>\d+),(?P<y1>\d+) -> (?P<x2>\d+),(?P<y2>\d+)", line
+            )
+            lines.append(
+                Line(
+                    Point(int(match.group("x1")), int(match.group("y1"))),
+                    Point(int(match.group("x2")), int(match.group("y2"))),
+                )
+            )
+        nonDiagonalLines = [line for line in lines if not line.isDiagonal]
+        points = collections.Counter()
+        for line in nonDiagonalLines:
+            points.update(line.points())
+        return len(list(itertools.takewhile(lambda i: i[1] >= 2, points.most_common())))
+
+
 if __name__ == "__main__":
-    print("hi")
+    print(process("input.txt"))
