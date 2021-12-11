72ff24b24a76fa70271d8a0a52eb3feb29a51758
Rob Johnson <Johnson.rob.alan@gmail.com>
2021-12-11 14:46:05 -0800

    day10 part 1

--- day10/syntax.py
+++ day10/syntax.py
@@ -1,2 +1,46 @@
+class NavigationSyntax:
+    def getLines(self, filename):
+        lines = []
+        with open(filename) as file:
+            for line in file:
+                lines.append(line.strip())
+        return lines
+
+    def __init__(self, filename):
+        self.unsorted = self.getLines(filename)
+        self.incomplete = []
+        self.corrupt = []
+        self.assessLines()
+
+    def assessLines(self):
+        opening = "({[<"
+        closing = ")}]>"
+        while self.unsorted:
+            line = self.unsorted.pop()
+            brackets = []
+            for c in line:
+                if c in opening:
+                    brackets.append(c)
+                else:
+                    if c == closing[opening.index(brackets[-1])]:
+                        brackets.pop()
+                    else:
+                        self.corrupt.append(c)
+                        break
+
+    def corruptScores(self):
+        return sum(self.getScore(c) for c in self.corrupt)
+
+    def getScore(self, c):
+        scores = {
+            ")": 3,
+            "]": 57,
+            "}": 1197,
+            ">": 25137,
+        }
+        return scores[c]
+
+
 if __name__ == "__main__":
-    print(5)
+    result = NavigationSyntax("input.txt").corruptScores()
+    print(result)
