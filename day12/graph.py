de2505609172d349f4da37c3f90b90ff2efabdc2
Rob Johnson <Johnson.rob.alan@gmail.com>
2021-12-13 21:44:49 -0800

    day12 part 2 solution

--- day12/graph.py
+++ day12/graph.py
@@ -14,27 +14,46 @@ class Caves:
             self.caves[a].add(b)
             self.caves[b].add(a)
 
-    def countPossiblePaths(self):
-        return self.pathsToStart("end", [])
+    def countPossiblePaths(self, extraVisitAllowed=False):
+        return self.pathsToStart("end", [], extraVisitAllowed)
 
-    def pathsToStart(self, location, visited):
+    def pathsToStart(self, location, visited, extraVisitAllowed):
         if location == "start":
             return 1
+        extraVisitAllowedForNext = extraVisitAllowed and (
+            location.isupper() or location not in visited
+        )
         remainingConnections = [
             nextLocation
             for nextLocation in self.caves[location]
-            if nextLocation.isupper() or nextLocation not in visited
+            if nextLocation != "end"
+            and (
+                nextLocation.isupper()
+                or nextLocation not in visited
+                or extraVisitAllowedForNext
+            )
         ]
         if not remainingConnections:
             return 0
         else:
-            ways = [
-                self.pathsToStart(nextLocation, visited + [location])
+            return sum(
+                self.pathsToStart(
+                    nextLocation,
+                    visited + [location],
+                    extraVisitAllowedForNext,
+                )
                 for nextLocation in remainingConnections
-            ]
-            return sum(ways)
+            )
 
 
 if __name__ == "__main__":
-    result = Caves("input.txt").countPossiblePaths()
-    print(result)
+    result = Caves("test.txt").countPossiblePaths(extraVisitAllowed=True)
+    print(result, 36)
+    result2 = Caves("test2.txt").countPossiblePaths(extraVisitAllowed=True)
+    print(result2, 103)
+    result3 = Caves("test3.txt").countPossiblePaths(extraVisitAllowed=True)
+    print(result3, 3509)
+    result4 = Caves("test4.txt").countPossiblePaths(extraVisitAllowed=True)
+    print(result4, 13)
+    result5 = Caves("input.txt").countPossiblePaths(extraVisitAllowed=True)
+    print(result5)
