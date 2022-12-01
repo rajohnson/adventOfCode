c5014bb8c1f5a7803ecc6810f40f75913dd3dfb5
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-01 13:38:02 -0800

    2022 day 1 part 1

---
+++ 2022/day1/app.py
@@ -0,0 +1,13 @@
+with open("input.txt", "r") as data_in:
+    inventory = []
+    all_inventories = []
+    for line in data_in:
+        if len(line) == 1:  # new elf
+            all_inventories.append(inventory)
+            inventory = []
+        else:
+            inventory.append(int(line))
+    if inventory:  # last one if the file doesn't have an extra newline
+        all_inventories.append(inventory)
+
+    print(max([sum(inventory) for inventory in all_inventories]))
