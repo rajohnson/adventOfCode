e51cd6c1a5c1b10ca3c70de4d3d0a7821ebf91c8
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-02 11:29:20 -0800

    clean up encoding

--- 2022/day2/app.py
+++ 2022/day2/app.py
@@ -1,82 +1,105 @@
-# A, X is rock
-# B, Y is paper
-# C, Z is scissors
-scoringB = {"X": 1, "Y": 2, "Z": 3}
-outcome_scores = {"W": 6, "D": 3, "L": 0}
-# X lost
-# Y draw
-# Z win
+player_conversion = {
+    "X": "rock",
+    "Y": "paper",
+    "Z": "scissors",
+    "A": "rock",
+    "B": "paper",
+    "C": "scissors",
+}
+outcome_conversion = {"X": "lose", "Y": "draw", "Z": "win"}
+scoring = {"rock": 1, "paper": 2, "scissors": 3, "lose": 0, "draw": 3, "win": 6}
 
 
 def game_result(playerA, playerB):
-    if playerA == "A":  # rock
-        if playerB == "X":  # rock
-            return "D"
-        elif playerB == "Y":  # paper
-            return "W"
+    if playerA == "rock":
+        if playerB == "rock":
+            return "draw"
+        elif playerB == "paper":
+            return "win"
         else:  # scissors
-            return "L"
-    elif playerA == "B":  # paper
-        if playerB == "X":  # rock
-            return "L"
-        elif playerB == "Y":  # paper
-            return "D"
+            return "lose"
+    elif playerA == "paper":
+        if playerB == "rock":
+            return "lose"
+        elif playerB == "paper":
+            return "draw"
         else:  # scissors
-            return "W"
+            return "win"
     else:  # scissors
-        if playerB == "X":  # rock
-            return "W"
-        elif playerB == "Y":  # paper
-            return "L"
+        if playerB == "rock":
+            return "win"
+        elif playerB == "paper":
+            return "lose"
         else:  # scissors
-            return "D"
+            return "draw"
 
 
 def round_part1(playerA, playerB) -> int:
-    win_score = outcome_scores[game_result(playerA, playerB)]
-    choice_score = scoringB[playerB]
+    win_score = scoring[game_result(playerA, playerB)]
+    choice_score = scoring[playerB]
     total = win_score + choice_score
     return total
 
 
 def play_for_outcome(playerA, outcome):
-    if outcome == "X":  # lose
-        if playerA == "A":  # rock
-            return "Z"
-        elif playerA == "B":  # paper
-            return "X"
+    if outcome == "lose":
+        if playerA == "rock":
+            return "scissors"
+        elif playerA == "paper":
+            return "rock"
         else:  # scissors
-            return "Y"
-    elif outcome == "Y":  # draw
-        if playerA == "A":  # rock
-            return "X"
-        elif playerA == "B":  # paper
-            return "Y"
+            return "paper"
+    elif outcome == "draw":
+        if playerA == "rock":
+            return "rock"
+        elif playerA == "paper":
+            return "paper"
         else:  # scissors
-            return "Z"
+            return "scissors"
     else:  # win
-        if playerA == "A":  # rock
-            return "Y"
-        elif playerA == "B":  # paper
-            return "Z"
+        if playerA == "rock":
+            return "paper"
+        elif playerA == "paper":
+            return "scissors"
         else:  # scissors
-            return "X"
+            return "rock"
 
 
 def round_part2(playerA, outcome) -> int:
     playerB = play_for_outcome(playerA, outcome)
-    win_score = outcome_scores[game_result(playerA, playerB)]
-    choice_score = scoringB[playerB]
+    win_score = scoring[game_result(playerA, playerB)]
+    choice_score = scoring[playerB]
     total = win_score + choice_score
     return total
 
 
-def main():
+def convert_symbols_to_plays_part1(line):
+    a, b = line.strip().split(" ")
+    return player_conversion[a], player_conversion[b]
+
+
+def convert_symbols_to_plays_part2(line):
+    a, outcome = line.strip().split(" ")
+    return player_conversion[a], player_conversion[outcome]
+
+
+def part1():
     with open("input.txt", "r") as file_in:
-        scores = [round_part2(*line.strip().split(" ")) for line in file_in]
-    print(scores)
+        scores = [
+            round_part1(*convert_symbols_to_plays_part1(line)) for line in file_in
+        ]
+    return sum(scores)
+
+
+def part2():
+    with open("input.txt", "r") as file_in:
+        scores = [
+            round_part2(*convert_symbols_to_plays_part2(line)) for line in file_in
+        ]
     return sum(scores)
 
 
 if __name__ == "__main__":
-    print(main())
+    print(f"p1 = 10994, p2 = 12526")
+    print(part1())
+    print(part2())
