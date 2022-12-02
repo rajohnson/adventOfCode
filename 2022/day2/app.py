98069f3e5f892236db532526df9c3de3a2578730
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-01 21:51:10 -0800

    day 2 part b

--- 2022/day2/app.py
+++ 2022/day2/app.py
@@ -1,9 +1,11 @@
 # A, X is rock
 # B, Y is paper
 # C, Z is scissors
-scoringA = {"A": 1, "B": 2, "C": 3}
 scoringB = {"X": 1, "Y": 2, "Z": 3}
 outcome_scores = {"W": 6, "D": 3, "L": 0}
+# X lost
+# Y draw
+# Z win
 
 
 def game_result(playerA, playerB):
@@ -30,7 +32,39 @@ def game_result(playerA, playerB):
             return "D"
 
 
-def round(playerA, playerB) -> int:
+def round_part1(playerA, playerB) -> int:
+    win_score = outcome_scores[game_result(playerA, playerB)]
+    choice_score = scoringB[playerB]
+    total = win_score + choice_score
+    return total
+
+
+def play_for_outcome(playerA, outcome):
+    if outcome == "X":  # lose
+        if playerA == "A":  # rock
+            return "Z"
+        elif playerA == "B":  # paper
+            return "X"
+        else:  # scissors
+            return "Y"
+    elif outcome == "Y":  # draw
+        if playerA == "A":  # rock
+            return "X"
+        elif playerA == "B":  # paper
+            return "Y"
+        else:  # scissors
+            return "Z"
+    else:  # win
+        if playerA == "A":  # rock
+            return "Y"
+        elif playerA == "B":  # paper
+            return "Z"
+        else:  # scissors
+            return "X"
+
+
+def round_part2(playerA, outcome) -> int:
+    playerB = play_for_outcome(playerA, outcome)
     win_score = outcome_scores[game_result(playerA, playerB)]
     choice_score = scoringB[playerB]
     total = win_score + choice_score
@@ -39,7 +73,7 @@ def round(playerA, playerB) -> int:
 
 def main():
     with open("input.txt", "r") as file_in:
-        scores = [round(*line.strip().split(" ")) for line in file_in]
+        scores = [round_part2(*line.strip().split(" ")) for line in file_in]
     print(scores)
     return sum(scores)
 
