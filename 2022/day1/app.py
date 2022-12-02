cbc2eb04b259570dc30798031762512eeef4a8e4
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-01 16:00:39 -0800

    break into sections and add alternate solutions.

--- 2022/day1/app.py
+++ 2022/day1/app.py
@@ -1,17 +1,52 @@
-with open("input.txt", "r") as data_in:
-    inventory = []
-    all_inventories = []
-    for line in data_in:
-        if len(line) == 1:  # new elf
+import re
+import collections
+
+
+def first_attempt():
+    with open("input.txt", "r") as data_in:
+        inventory = []
+        all_inventories = []
+        for line in data_in:
+            if len(line) == 1:  # new elf
+                all_inventories.append(inventory)
+                inventory = []
+            else:
+                inventory.append(int(line))
+        if inventory:  # last one if the file doesn't have an extra newline
             all_inventories.append(inventory)
-            inventory = []
-        else:
-            inventory.append(int(line))
-    if inventory:  # last one if the file doesn't have an extra newline
-        all_inventories.append(inventory)
 
     part1_solution = max([sum(inventory) for inventory in all_inventories])
     part2_solution = sum(sorted([sum(inventory) for inventory in all_inventories])[-3:])
 
-    print(f"{part1_solution=}")
-    print(f"{part2_solution=}")
+    return part1_solution, part2_solution
+
+
+def second_attempt():
+    with open("input.txt", "r") as data_in:
+        groups = re.split(r"(?:\r?\n){2,}", data_in.read())
+    all_inventories = [[int(items) for items in group.split()] for group in groups]
+
+    part1_solution = max([sum(inventory) for inventory in all_inventories])
+    part2_solution = sum(sorted([sum(inventory) for inventory in all_inventories])[-3:])
+    return part1_solution, part2_solution
+
+
+def third_attempt():
+    all_inventories = [0]
+    with open("input.txt", "r") as data_in:
+        for line in data_in:
+            stripped = line.strip()
+            if stripped:
+                all_inventories[-1] += int(stripped)
+            else:
+                all_inventories.append(0)
+
+        part1_solution = max(all_inventories)
+        part2_solution = sum(sorted(all_inventories)[-3:])
+        return part1_solution, part2_solution
+
+
+if __name__ == "__main__":
+    print(first_attempt())
+    print(second_attempt())
+    print(third_attempt())
