94fb6b058542b439922805b7c1874e3ea750e98c
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-02 22:49:48 -0800

    day 3 part 2

--- 2022/day3/app.py
+++ 2022/day3/app.py
@@ -17,5 +17,17 @@ def part1():
     return sum(data)
 
 
+def part2():
+    with open("input.txt", "r") as file_in:
+        lines = [line.strip() for line in file_in.readlines()]
+        groups = zip(lines[0::3], lines[1::3], lines[2::3])
+        priority_scores = [
+            get_priority(set(a).intersection(b).intersection(c).pop())
+            for a, b, c in groups
+        ]
+    return sum(priority_scores)
+
+
 if __name__ == "__main__":
     print(part1())
+    print(part2())
