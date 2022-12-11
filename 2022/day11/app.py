65e3748955ef9de66f4c263263528ac9daa70b50
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-10 22:33:37 -0800

    day11 part1

---
+++ 2022/day11/app.py
@@ -0,0 +1,67 @@
+import dataclasses
+import re
+
+
+@dataclasses.dataclass
+class Monkey:
+    id: int
+    items: list[int]
+    operation: str
+    divisibility_num: int
+    dest_true: int
+    dest_false: int
+    inspections: int = 0
+
+    @classmethod
+    def from_str(cls, monkey_str: str):
+        monkey_match = re.match(
+            (
+                r"Monkey (\d)+:\n  Starting items: (.*)\n  Operation: (.*)\n"
+                r"  Test: divisible by (\d+)\n    If true: throw to monkey"
+                r" (\d+)\n    If false: throw to monkey (\d+)"
+            ),
+            monkey_str,
+        )
+        id, items, operation, divisibility_num, dest_true, dest_false = [
+            monkey_match.group(i + 1) for i in range(6)
+        ]
+        return cls(
+            id=int(id),
+            items=[int(item) for item in items.split(",")],
+            operation=operation,
+            divisibility_num=int(divisibility_num),
+            dest_true=int(dest_true),
+            dest_false=int(dest_false),
+        )
+
+
+def part1(filename: str):
+    with open(filename, "r") as file_in:
+        data = file_in.read()
+    data = data.replace("old", "item").replace("new = ", "")
+    monkeys = [
+        Monkey.from_str(monkey_str) for monkey_str in data.split("\n\n")
+    ]
+    for round in range(20):
+        for monkey in monkeys:
+            for item in monkey.items:
+                monkey.inspections += 1
+                item = eval(monkey.operation, {}, {"item": item})
+                # item = eval(monkey.operation)
+                item = item // 3
+                if item % monkey.divisibility_num == 0:
+                    monkeys[monkey.dest_true].items.append(item)
+                else:
+                    monkeys[monkey.dest_false].items.append(item)
+            monkey.items = []
+    a, b = sorted([monkey.inspections for monkey in monkeys])[-2:]
+    return a * b
+
+
+def part2(filename: str):
+    return None
+
+
+if __name__ == "__main__":
+    print(f"{part1('input.txt')=}")
+    print(f"{part2('input.txt')=}")
