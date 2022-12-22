0491e68aaf45ff07093158f49f4bf0b01ed6c801
Rob Johnson <Johnson.rob.alan@gmail.com>
2022-12-21 21:04:42 -0800

    day20 part1

--- 2022/day20/app.py
+++ 2022/day20/app.py
@@ -7,10 +7,20 @@ Entry = collections.namedtuple("Entry", "value id")
 def part1(filename: str):
     with open(filename, "r") as file_in:
         data = [Entry(int(num), id) for id, num in enumerate(file_in.readlines())]
-    # for id in range(len(data)):
-    for id in range(1):
-        ...
-    return [d.id for d in data]
+        id_queue = data.copy()
+    for entry in id_queue:
+        index = data.index(entry)
+        data.pop(index)
+        while index + entry.value < 0:
+            index += len(data)
+        insert_index = (index + entry.value) % len(data)
+        data.insert(insert_index, entry)
+    obj0 = [obj for obj in data if obj.value == 0].pop()
+    index0 = data.index(obj0)
+    index1000 = (index0 + 1000) % len(data)
+    index2000 = (index0 + 2000) % len(data)
+    index3000 = (index0 + 3000) % len(data)
+    return sum([data[index1000].value, data[index2000].value, data[index3000].value])
 
 
 def part2(filename: str):
@@ -19,4 +29,5 @@ def part2(filename: str):
 
 if __name__ == "__main__":
     print(f"{part1('example.txt')=}")
+    print(f"{part1('input.txt')=}")
     print(f"{part2('example.txt')=}")
