de1d10aa8f5dda587dba0da647c9e4e5e0f639b0
Rob Johnson <johnson.rob.alan@gmail.com>
2023-12-05 18:27:38 -0800

    start of day 1 part 2

--- 2023/day1/app.py
+++ 2023/day1/app.py
@@ -12,15 +12,26 @@ def calibrate(line: str) -> int:
     return int(first) * 10 + int(last)
 
 
+# find the first and last digit in a string and make them into a two digit number digits can be actual digits or the english word for the digit
+def calibrate_text(line: str) -> int:
+    raise NotImplementedError
+
+
 # get sum of the calibration value for each line in file_name
-def attempt(file_name: str) -> int:
+def attempt(file_name: str, part: int) -> int:
     total = 0
     with open(file_name, "r") as data_in:
         for line in data_in:
-            total += calibrate(line)
+            if part == 1:
+                total += calibrate(line)
+            elif part == 2:
+                total += calibrate_text(line)
+            else:
+                raise ValueError(f"part must be 1 or 2, not {part}")
 
     return total
 
 
 if __name__ == "__main__":
-    print(f'{attempt("input.txt")=}')
+    print(f'{attempt("input.txt", 1)=}')
+    print(f'{attempt("input.txt", 2)=}')
--- 2023/day1/test_app.py
+++ 2023/day1/test_app.py
@@ -8,5 +8,16 @@ def test_calibrate():
     assert app.calibrate("treb7uchet") == 77
 
 
+def test_calibrate_text():
+    assert app.calibrate_text("two1nine") == 29
+    assert app.calibrate_text("eightwothree") == 83
+    assert app.calibrate_text("abcone2threexyz") == 13
+    assert app.calibrate_text("xtwone3four") == 24
+    assert app.calibrate_text("4nineeightseven2") == 42
+    assert app.calibrate_text("zoneight234") == 14
+    assert app.calibrate_text("7pqrstsixteen") == 76
+
+
 def test_attempt():
-    assert app.attempt("example.txt") == 142
+    assert app.attempt("example.txt", 1) == 142
+    assert app.attempt("example.txt", 2) == 281
